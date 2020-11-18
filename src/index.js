34f850c2237f028fc23b51ad1bd65eefa4d2275d
Collins Uzebu <uzebuc@gmail.com>
2020-11-18 17:05:22 +0000

    new dependencies added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "semantic-ui-css/semantic.min.css";
+import { Provider } from "react-redux";
+import configStore from "./redux/configStore";
+
+const store = configStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
