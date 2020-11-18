5ce1fc453ded3c3b03d8017a0b1d63fd9cbb17ed
Collins Uzebu <uzebuc@gmail.com>
2020-11-18 17:01:36 +0000

    redirect users to application when they are logged in

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch, useHistory } from "react-router-dom";
+import firebase from "./firebase";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import HomePage from "./components/HomePage";
+import { clearUser, setUser } from "./redux/actions/user";
+import Spinner from "./Spinner";
 
-const BaseRouter = () => (
-  <Router>
+const BaseRouter = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.user.isLoading);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(setUser(user));
+        history.push("/");
+      } else {
+        dispatch(clearUser());
+        history.push("/login");
+      }
+    });
+  }, []);
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
     </Switch>
-  </Router>
-);
+  );
+};
 
 export default BaseRouter;
