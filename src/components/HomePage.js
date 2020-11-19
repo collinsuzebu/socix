0ac761a9d3f5b147364563c760729811b6984dc4
Collins Uzebu <uzebuc@gmail.com>
2020-11-19 15:01:48 +0000

    homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Grid } from "semantic-ui-react";
 import { ColorPanel } from "./color-panel/ColorPanel";
 import { Messages } from "./messages/Messages";
@@ -6,10 +7,16 @@ import { MetaPanel } from "./meta-panel/MetaPanel";
 import { SidePanel } from "./side-panel/SidePanel";
 
 export default function HomePage() {
+  const color = useSelector((state) => state.color);
+
   return (
-    <Grid columns="equal" className="app" style={{ background: "#eee" }}>
-      <ColorPanel />
-      <SidePanel />
+    <Grid
+      columns="equal"
+      className="app"
+      style={{ background: color.secondary }}
+    >
+      <ColorPanel color={color} />
+      <SidePanel color={color} />
 
       <Grid.Column style={{ marginLeft: 320 }}>
         <Messages />
