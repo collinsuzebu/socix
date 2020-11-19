2b4a0c0de335d50879ef4855c454cbf908785301
Collins Uzebu <uzebuc@gmail.com>
2020-11-19 14:49:23 +0000

    added  color to initial state

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import user from "./user";
 import channel from "./channel";
+import color from "./color";
 
 const mainReducer = combineReducers({
   user,
   channel,
+  color,
 });
 
 export default mainReducer;
--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,5 +1,6 @@
 export default {
   currentUser: null,
   channel: { currentChannel: null, isPrivateChannel: false },
+  color: { primary: "#4c3c4c", secondary: "#fff" },
   isLoading: true,
 };
