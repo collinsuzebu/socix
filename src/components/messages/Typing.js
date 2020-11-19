5f3c307ca6d87f5bea63852c1fb9359def57132c
Collins Uzebu <uzebuc@gmail.com>
2020-11-19 16:16:09 +0000

    typing user component

---
+++ src/components/messages/Typing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Typing() {
+  return (
+    <div className="typing">
+      <div className="typing__dot"></div>
+      <div className="typing__dot"></div>
+      <div className="typing__dot"></div>
+    </div>
+  );
+}
