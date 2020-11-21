ee554c29f6cec76d0ea2d2bee7c8aa227717d6fb
Collins Uzebu <uzebuc@gmail.com>
2020-11-21 16:34:03 +0000

    spinner component to receive props

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Dimmer, Loader } from "semantic-ui-react";
 
-export default function Spinner() {
+export default function Spinner({ size, content }) {
   return (
     <Dimmer active inverted>
-      <Loader size="massive" content={"Setting up your workspace"} />
+      <Loader
+        size={size ? size : "massive"}
+        content={content ? content : "Setting up your workspace"}
+      />
     </Dimmer>
   );
 }
