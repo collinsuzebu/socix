445dca8e088029e4fb2435fd03ac56f2eeae89e0
Collins Uzebu <uzebuc@gmail.com>
2020-11-21 16:38:51 +0000

    searched result added to combine reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import user from "./user";
 import channel from "./channel";
 import color from "./color";
+import searched from "./search";
 
 const mainReducer = combineReducers({
   user,
   channel,
   color,
+  searched,
 });
 
 export default mainReducer;
