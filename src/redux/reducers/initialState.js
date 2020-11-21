8eaac5cac72ed0f82db001fb99beb7a991a5cef7
Collins Uzebu <uzebuc@gmail.com>
2020-11-21 16:32:28 +0000

    set initial state for search results

--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,6 +1,11 @@
 export default {
   currentUser: null,
   channel: { currentChannel: null, isPrivateChannel: false },
-  color: { primary: "#4c3c4c", secondary: "#fff" },
+  color: {
+    primary: "#0D7E83",
+    secondary: "#fff",
+    default: { primary: "#0D7E83", secondary: "#fff" },
+  },
+  searchResults: [],
   isLoading: true,
 };
